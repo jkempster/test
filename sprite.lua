99f890b788e63afca98e5c49db239c2555df1570
Jason Kempster <jason.kempster@gmail.com>
2011-12-13 17:10:03 +0000

    testing

--- sprite.lua
+++ sprite.lua
@@ -2,12 +2,7 @@
 -- Created by IntelliJ IDEA.
 -- User: jason.kempster
 -- Date: 13/12/2011
--- Time: 17:03
+-- Time: 17:09
 -- To change this template use File | Settings | File Templates.
 --
-function start()
-	display.createSprite({source = "icon.png"})
-end
 
-function theEnd()
-end
