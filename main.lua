8212eeb32901133ce4ace9b3f8555d2646fda2c7
Jason Kempster <jason.kempster@gmail.com>
2011-12-13 17:07:38 +0000

    testing

---
+++ main.lua
@@ -0,0 +1,10 @@
+---
+-- Created by IntelliJ IDEA.
+-- User: jason.kempster
+-- Date: 13/12/2011
+-- Time: 17:03
+-- To change this template use File | Settings | File Templates.
+--
+function start()
+	display.createSprite({source = "icon.png"})
+end
